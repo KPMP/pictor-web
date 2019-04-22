ba15725c376fc8db24fcbca3f8c770792c10cd54
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2019-04-22 13:06:59 -0400

    KPMP-873 extracted data path-related logic to API helper; added download button behavior

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -6,4 +6,27 @@ export default class Api {
       timeout: 10000,
     });
   }
+
+  static getDatasetGeneViolinPlotFilename(dataset, gene) {
+    let filename = dataset + "_violinPlot.csv";
+    return "/data/gene-expression/" + gene.charAt(0) + "/" + gene + "/" + filename;
+  }
+
+  static getDownloadReadyViolinPlotPath(gene) {
+    return "/data/gene-expression/" + gene.charAt(0) + "/" + gene + "/" +
+        gene + "_expression.csv";
+  }
+
+    static getDownloadReadyViolinPlotFilename(gene) {
+        let today = new Date(),
+            dd = today.getDate(),
+            mm = today.getMonth() + 1,
+            yyyy = today.getFullYear();
+
+        dd = dd < 10 ? "0" + dd : dd;
+        mm = mm < 10 ? "0" + mm : mm;
+
+        return `${gene}_expression_${yyyy}${mm}${dd}.csv`;
+    }
+
 }
