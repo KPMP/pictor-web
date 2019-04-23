823f876a0cb59a4d999fab2cacde2131e054d23c
Becky Reamy <rlreamy@umich.edu>
2019-04-23 11:35:51 -0400

    KPMP-929: Dumb summary page

--- src/components/GeneSummary/GeneSummaryPage.js
+++ src/components/GeneSummary/GeneSummaryPage.js
@@ -20,37 +20,7 @@ class GeneSummaryPage extends Component {
     render() {
         return (
             <Container id="gene-summary-page">
-                <GeneSearchFormContainer/>
-                <Row id="gene-summary-page-header">
-                    <Col xs="12" sm="12">
-                        <div>
-                        <span id="gene-summary-page-header-text" className="float-left">{this.props.geneSymbol} expression by cell type across methods</span>
-                        <span onClick={() => this.trackDownload(this.props.geneSymbol)}>
-	                        <Button tag="a" className="float-right" color="primary"
-	                            href={Api.getDownloadReadyViolinPlotPath(this.props.geneSymbol)}
-	                            download={Api.getDownloadReadyViolinPlotFilename(this.props.geneSymbol)}
-	                            >Download</Button>
-                        </span>
-                        </div>
-                    </Col>
-                </Row>
-                
-                <Row id="gene-summary-plots">
-                    <Col xs="12" sm="12">
-                        <GeneSummaryViolinPlotContainer datasetName={"MDSCRNA-SEQ"} tisName={"(UCSF/Stanford)"} />
-                        <GeneSummaryViolinPlotContainer datasetName={"SCRNA-SEQ"} tisName={"(Michigan/Broad/Princeton)"} />
-                        <GeneSummaryViolinPlotContainer datasetName={"SNDROP-SEQ"} tisName={"(UCSD/WashU)"} />
-                    </Col>
-                </Row>
-                <Row id="gene-summary-legend-row">
-	                <Col xs="12">
-		                <Card id="gene-summary-legend">
-			                <CardBody>
-			                	<GeneSummaryLegend/>
-			                </CardBody>
-		                </Card>
-	                </Col>
-                </Row>
+              HI
             </Container>
         )
     }
